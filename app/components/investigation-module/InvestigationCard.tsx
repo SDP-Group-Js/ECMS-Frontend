2a038fcbc4267ed8e40409af5ca0e0658aba1a09
Sasvidu Ranthul <66816946+Sasvidu@users.noreply.github.com>
2023-12-16 15:10:30 +0530

    Build Allocate Investigation Modal

--- app/components/investigation-module/InvestigationCard.tsx
+++ app/components/investigation-module/InvestigationCard.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import InvestigationStatusTag from "./InvestigationStatusTag";
+import AllocateInvestigationModal from "./AllocateInvestigationModal";
 
 type props = {
   investigationId: number;
@@ -18,24 +19,55 @@ const InvestigationCard = ({
   investigationOfficeName,
   investigationUpdateDate,
 }: props) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleStatusButtonClick = () => {
+    setModalVisible(true);
+  };
+
+  const handleModalCloseButtonClick = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <div className="my-2 flex items-center justify-between rounded-xl border-2 border-gray-400 px-4 py-[0.75rem]">
-      <div>{investigationId}</div>
-      <div className="flex items-center justify-center space-x-10">
-        <div
-          className={investigationStatus === "NotAssigned" ? "hidden" : "block"}
-        >
-          {investigationOfficeType}: {investigationOfficeName}
+    <>
+      <div className="my-2 flex items-center justify-between rounded-xl border-2 border-gray-400 px-4 py-[0.75rem]">
+        <div>{investigationId}</div>
+        <div className="flex items-center justify-center space-x-10">
+          <div
+            className={
+              investigationStatus === "NotAssigned" ? "hidden" : "block"
+            }
+          >
+            {investigationOfficeType}: {investigationOfficeName}
+          </div>
+          <div className="w-30 flex items-center justify-center">
+            <InvestigationStatusTag
+              investigationId={investigationId}
+              investigationStatus={investigationStatus}
+              onClick={handleStatusButtonClick}
+            />
+          </div>
+          <div>{investigationUpdateDate.toLocaleDateString()}</div>
         </div>
-        <div className="w-30 flex items-center justify-center">
-          <InvestigationStatusTag
-            investigationId={investigationId}
-            investigationStatus={investigationStatus}
-          />
-        </div>
-        <div>{investigationUpdateDate.toLocaleDateString()}</div>
       </div>
-    </div>
+      {modalVisible && investigationStatus === "NotAssigned" && (
+        <AllocateInvestigationModal
+          isVisible={modalVisible}
+          investigationId={investigationId}
+          officeId={investigationOfficeId}
+          handleCloseButtonClick={handleModalCloseButtonClick}
+        />
+      )}
+      {modalVisible && investigationStatus === "Ongoing" && (
+        <AllocateInvestigationModal
+          isVisible={modalVisible}
+          investigationId={investigationId}
+          officeId={investigationOfficeId}
+          handleCloseButtonClick={handleModalCloseButtonClick}
+        />
+      )}
+    </>
   );
 };
 
