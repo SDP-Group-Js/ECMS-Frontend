fb2c565a3a7e78d911b220b2a4aedbaa25a87657
Sheneli2006 <152201310+Sheneli2006@users.noreply.github.com>
2023-12-17 11:25:49 +0530

    Commititng new chnages

--- app/Institutions/page.tsx
+++ app/Institutions/page.tsx
@@ -5,6 +5,7 @@ import Sidebarn from '../components/shared/Sidebarn'
 import InstmHeader from '../components/institution-module/institution-man/InstmHeader'
 import ManInst from '../components/institution-module/institution-man/Manage-Institutions';
 import './index.css';
+import DashboardContentPanel from '../components/shared/DashboardContentPanel';
 
 export default function Home(){
     return (
@@ -12,9 +13,10 @@ export default function Home(){
             <Sidebarn />
             <div className='flex-1'>
                 <InstmHeader />
-                <div className='p-4'><ManInst /></div>
+                <div className='p-4'><DashboardContentPanel /></div>
             </div>
         </div>
       );
-    
+
 }
+//changeee
