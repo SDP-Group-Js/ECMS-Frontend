0ef08a5eec0986725e9444f96c21bd4cd12c0279
NMProgrammer <nathanwijayasekara@gmail.com>
2023-12-17 09:00:23 +0530

    Update

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Modal = ({ onClick, children, color }: any) => {
     return (
-        <div onClick={onClick} style={{color: `${color} !important`}} className="absolute overflow-x-hidden overflow-y-auto flex justify-center items-center top-0 left-0 z-50 bg-[#00000096] w-full h-full">
+        <div onClick={onClick} style={{color: `${color} !important`}} className="fixed overflow-x-hidden overflow-y-auto flex justify-center items-center top-0 left-0 z-50 bg-[#00000096] w-full h-full">
             {children}
         </div>
     )
--- app/workflows/page.tsx
+++ app/workflows/page.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import CreateInstitutionWorkflow from "../components/workflow-module/CreateInstitutionWorkflow";
 import ViewInstitutionWorkflows from "../components/workflow-module/ViewInstitutionWorkflows";
 import ViewInvestigationActions from "../components/investigation-module/ViewInvestigationActions";
+import SetInvestigationWorkflow from "../components/workflow-module/SetInvestigationWorkflow";
+import dumbyData from "../../tests/dumbyData.json";
 
 const WorkflowPage = () => {
   const sampleInstitution = {
@@ -11,6 +13,7 @@ const WorkflowPage = () => {
   };
 
   const [openCreateInstitutionWorkflowForm, setOpenCreateInstitutionWorkflowForm] = useState(false)
+  const [openSetInvestigationWorkflowForm, setOpenSetInvestigationWorkflowForm] = useState(false)
 
   return (
     <div>
@@ -23,6 +26,14 @@ const WorkflowPage = () => {
         institutionName={sampleInstitution.name}
         closeForm={() => setOpenCreateInstitutionWorkflowForm(false)}
       />
+      )}
+      <button onClick={() => setOpenSetInvestigationWorkflowForm(true)} className="px-2 py-1 rounded bg-gray-600 text-white font-bold">Set Investigation Workflow</button>
+      {openSetInvestigationWorkflowForm && (
+        <SetInvestigationWorkflow
+          investigationId="3290w9dfsi"
+          closeForm={() => setOpenSetInvestigationWorkflowForm(false)} 
+          institutionWorkflows={dumbyData.institutionWorkflows}      
+        />
       )}
     </div>
   );
